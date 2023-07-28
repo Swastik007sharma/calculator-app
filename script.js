904c5ee5f1cd4c9cdb9d949d2912950d031b3571
Swastik <Swastik007sharma@gmail.com>
2023-07-28 19:48:38 +0530

    Theme changing feature completed

---
+++ script.js
@@ -0,0 +1,61 @@
+let toggleSwitch = document.getElementsByClassName('themeSwitch')[0]
+
+function go_to_1() {
+    toggleSwitch.classList.add('horizTranslate1');
+    toggleSwitch.classList.remove('horizTranslate2');
+    toggleSwitch.classList.remove('horizTranslate3');
+    changeTheme(1)
+}
+
+function go_to_2() {
+    toggleSwitch.classList.add('horizTranslate2');
+    toggleSwitch.classList.remove('horizTranslate3');
+    toggleSwitch.classList.remove('horizTranslate1');
+    changeTheme(2)
+}
+
+function go_to_3() {
+    toggleSwitch.classList.add('horizTranslate3');
+    toggleSwitch.classList.remove('horizTranslate2');
+    toggleSwitch.classList.remove('horizTranslate1');
+    changeTheme(3)
+}
+
+function changeTheme(n) {
+    /* Changing main background and color */
+    document.body.style.background = `var(--T${n}_MainBackground`;
+    document.body.style.color = `var(--T${n}_PrimaryTextColor`;
+
+    /* Changing background of Output Screen */
+    document.querySelector('.output').style.backgroundColor = `var(--T${n}_ScreenBackgroundColor)`;
+
+    /* Changing background of input pad*/
+    document.querySelector('.inputPad').style.backgroundColor = `var(--T${n}_KeypadBackgroundColor)`
+
+    /* Changing buttons background and color */
+    document.querySelectorAll(".button").forEach((curr) => {
+        curr.style.backgroundColor = `var(--T${n}_NumberKeyBackground)`;
+        curr.style.color = `var(--T${n}_SecondTextColor)`;
+        curr.style.boxShadow = `inset 0 -4px var(--T${n}_NumberKeyShadow)`; 
+    });
+
+    /* Changing color, box shadow and background of reset and  del key */
+    document.getElementById('reset').style.backgroundColor = `var(--T${n}_KeyBackground)`;
+    document.getElementById('reset').style.color = `var(--T${n}_ThirdTextColor)`;
+    document.getElementById('reset').style.boxShadow = `inset 0 -4px var(--T${n}_KeyShadow)`;
+    
+    document.getElementById('del').style.backgroundColor = `var(--T${n}_KeyBackground)`;
+    document.getElementById('del').style.color = `var(--T${n}_ThirdTextColor)`;
+    document.getElementById('del').style.boxShadow = `inset 0 -4px var(--T${n}_KeyShadow)`;
+
+    /* Changing color, box shadow and background of reset and  del key */
+    document.getElementById('equall').style.backgroundColor = `var(--T${n}_ToggleKeyColor)`;
+    document.getElementById('equall').style.color = `var(--T${n}_ForthTextColor)`;
+    document.getElementById('equall').style.boxShadow = `inset 0 -4px var(--T${n}_ToggleKeyShadow)`;
+
+    /* Changing toggle Switch background */
+    document.getElementById('buttonContainer').style.backgroundColor = `var(--T${n}_ToggleBackgroundColor)`;
+
+    /* Changing toggle Switch color */
+    document.querySelector('.themeSwitch').style.backgroundColor = `var(--T${n}_ToggleKeyColor)`
+}
